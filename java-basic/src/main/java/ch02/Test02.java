c38d297d6831294313a70d1e09f561ddb66e8d5f
minheejeon <yycel1357@naver.com>
2019-01-03 18:25:01 +0900

    2019-01-03 학습

---
+++ java-basic/src/main/java/ch02/Test02.java
@@ -0,0 +1,23 @@
+package ch02;
+
+public class Test02 {
+  public static void main(String[] args) {
+    
+    // 20억을 4바이트 리터럴로 출력하라
+    
+    System.out.println(2000000000);
+    
+    // 20억을 8바이트 리터럴로 출력하라
+    
+    System.out.println(2000000000L);
+    
+    // 30억을 8바이트 리터럴로 출력하라
+    
+    System.out.println(3000000000L);
+    
+    // 30억을 4바이트 리터럴로 출력하라
+    
+    // System.out.println(3000000000); int의 범위를 벗어나 컴파일 오류가 일어남.
+    
+    }
+}
