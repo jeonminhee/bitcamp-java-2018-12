0c68a3ed1ee30edd86f7cb56ea7591aabe14378d
minheejeon <yycel1357@naver.com>
2019-03-21 09:42:15 +0900

    .

---
+++ java-basic/src/main/java/ch30/a/Test01.java
@@ -0,0 +1,24 @@
+// 
+package ch30.a;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test01 {
+
+  public static void main(String[] args) {
+
+    ApplicationContext iocContainer
+      = new ClassPathXmlApplicationContext("ch29/i/application-context-01.xml");
+
+    System.out.println("-------------------------------------------------------------------------------");
+    
+    String[] names = iocContainer.getBeanDefinitionNames();
+    for(String name : names) {
+      System.out.printf("%s ===> %s\n", 
+          name, iocContainer.getBean(name).getClass().getName());
+    }
+    
+    
+  }
+}
