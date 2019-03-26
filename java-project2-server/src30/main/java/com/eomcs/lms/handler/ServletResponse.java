ca70322478a168901ab54aeb3badbcd4d4da075b
minheejeon <yycel1357@naver.com>
2019-03-26 18:15:02 +0900

    tomcat

--- java-project2-server/src30/main/java/com/eomcs/lms/handler/ServletResponse.java
+++ java-project2-server/src30/main/java/com/eomcs/lms/handler/ServletResponse.java
@@ -5,19 +5,18 @@ import java.io.PrintWriter;
 import java.sql.Date;
 
 public class ServletResponse {
-
+  
   BufferedReader in;
   PrintWriter out;
-
+  
   public ServletResponse(BufferedReader in, PrintWriter out) {
     this.in = in;
     this.out = out;
   }
-
+  
   public PrintWriter getWriter() {
     return out;
   }
-  
   
   public void println(String message) {
     out.println(message);
@@ -30,10 +29,24 @@ public class ServletResponse {
     out.flush();
     return in.readLine();
   }
+  
   public int requestInt(String title) throws Exception {
     return Integer.parseInt(this.requestString(title));
   }
+  
   public Date requestDate(String title) throws Exception {
     return Date.valueOf(this.requestString(title));
-  }
+  }  
 }
+
+
+
+
+
+
+
+
+
+
+
+
