df0b15d936458fe9ab6ddc3afd3d0ca3c22fb6e7
minheejeon <yycel1357@naver.com>
2019-03-28 20:05:18 +0900

    servlet

---
+++ java-web/src/main/java/bitcamp/ex04/Servlet03.java
@@ -0,0 +1,47 @@
+// 클라이언트가 보낸 데이터 읽기 - POST 요청 데이터 읽기
+package bitcamp.ex04;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/ex04/s3")
+public class Servlet03 extends GenericServlet {
+  
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void service(ServletRequest req, ServletResponse res)
+      throws ServletException, IOException {
+    
+    // POST 요청
+    // - 웹 페이지의 폼(method='POST' 일 때)에서 전송 버튼을 클릭하면 POST 요청을 보낸다.
+    //
+    // 테스트
+    // - http://localhost:8080/java-web/ex04/test03.html 실행
+    //
+    req.setCharacterEncoding("UTF-8");
+
+    int age = Integer.parseInt(req.getParameter("age"));
+    String name = req.getParameter("name");
+    String photo = req.getParameter("photo");
+    
+    res.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = res.getWriter();
+    out.printf("이름=%s\n", name);
+    out.printf("나이=%d\n", age);
+    out.printf("사진=%s\n", photo);
+  }
+}
+
+
+
+
+
+
+
+
---
+++ java-web/src/main/java/bitcamp/ex04/Servlet04.java
@@ -0,0 +1,43 @@
+// 클라이언트가 보낸 데이터 읽기 - POST 요청 데이터 읽기
+package bitcamp.ex04;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/ex04/s4")
+public class Servlet04 extends GenericServlet {
+  
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void service(ServletRequest req, ServletResponse res)
+      throws ServletException, IOException {
+    
+    // 테스트
+    // - http://localhost:8080/java-web/ex04/test04.html 실행
+    //
+    req.setCharacterEncoding("UTF-8");
+
+    String age = req.getParameter("age");
+    String name = req.getParameter("name");
+    String photo = req.getParameter("photo");
+    
+    res.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = res.getWriter();
+    out.printf("이름=%s\n", name);
+    out.printf("나이=%s\n", age);
+    out.printf("사진=%s\n", photo);
+  }
+}
+
+
+
+
+
+
+
