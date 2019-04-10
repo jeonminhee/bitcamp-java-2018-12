4ef1101d26b34d30aa365d7c3166a362f74a75e5
minheejeon <yycel1357@naver.com>
2019-04-10 12:24:25 +0900

    pageController

---
+++ java-web-project/src/main/java/com/eomcs/lms/controller/BoardDeleteServlet.java
@@ -0,0 +1,34 @@
+package com.eomcs.lms.controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import com.eomcs.lms.service.BoardService;
+
+@Controller("/board/delete")
+public class BoardDeleteServlet implements PageController {
+  
+  @Autowired BoardService boardService;
+
+  @Override
+  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    int no = Integer.parseInt(request.getParameter("no"));
+
+    if (boardService.delete(no) == 0) {
+      throw new Exception("해당 번호의 게시글이 없습니다.");
+    } 
+    return "redirect:list";
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
